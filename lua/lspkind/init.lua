6c1e34332fa650e19deb4f99ebfc173194bd8ffa
eeeXun <sdes96303@gmail.com>
2025-11-04 21:57:29 +0800

    refactor: clean unused code

--- lua/lspkind/init.lua
+++ lua/lspkind/init.lua
@@ -1,5 +1,4 @@
 local lspkind = {}
-local fmt = string.format
 
 local kind_presets = {
   default = {
@@ -60,75 +59,6 @@ local kind_presets = {
   },
 }
 
-local kind_order = {
-  "Text",
-  "Method",
-  "Function",
-  "Constructor",
-  "Field",
-  "Variable",
-  "Class",
-  "Interface",
-  "Module",
-  "Property",
-  "Unit",
-  "Value",
-  "Enum",
-  "Keyword",
-  "Snippet",
-  "Color",
-  "File",
-  "Reference",
-  "Folder",
-  "EnumMember",
-  "Constant",
-  "Struct",
-  "Event",
-  "Operator",
-  "TypeParameter",
-}
-local kind_len = 25
-
-local function get_symbol(kind)
-  local symbol = lspkind.symbol_map[kind]
-  return symbol or ""
-end
-
-local modes = {
-  ["text"] = function(kind)
-    return kind
-  end,
-  ["text_symbol"] = function(kind)
-    local symbol = get_symbol(kind)
-    return fmt("%s %s", kind, symbol)
-  end,
-  ["symbol_text"] = function(kind)
-    local symbol = get_symbol(kind)
-    return fmt("%s %s", symbol, kind)
-  end,
-  ["symbol"] = function(kind)
-    local symbol = get_symbol(kind)
-    return fmt("%s", symbol)
-  end,
-}
-
--- default true
--- deprecated
-local function opt_with_text(opts)
-  return opts == nil or opts["with_text"] == nil or opts["with_text"]
-end
-
--- default 'symbol'
-local function opt_mode(opts)
-  local mode = "symbol"
-  if opt_with_text(opts) and opts ~= nil and opts["mode"] == nil then
-    mode = "symbol_text"
-  elseif opts ~= nil and opts["mode"] ~= nil then
-    mode = opts["mode"]
-  end
-  return mode
-end
-
 -- default 'default'
 local function opt_preset(opts)
   local preset
@@ -141,46 +71,20 @@ local function opt_preset(opts)
 end
 
 function lspkind.init(opts)
-  if opts ~= nil and opts["with_text"] ~= nil then
-    vim.api.nvim_command("echoerr 'DEPRECATED replaced by mode option.'")
-  end
   local preset = opt_preset(opts)
 
   local symbol_map = kind_presets[preset]
   lspkind.symbol_map = (opts and opts["symbol_map"] and vim.tbl_extend("force", symbol_map, opts["symbol_map"]))
     or symbol_map
-
-  local symbols = {}
-  local len = kind_len
-  for i = 1, len do
-    local name = kind_order[i]
-    symbols[i] = lspkind.symbolic(name, opts)
-  end
-
-  for k, v in pairs(symbols) do
-    require("vim.lsp.protocol").CompletionItemKind[k] = v
-  end
 end
 
 lspkind.setup = lspkind.init
 lspkind.presets = kind_presets
 lspkind.symbol_map = kind_presets.default
 
-function lspkind.symbolic(kind, opts)
-  local mode = opt_mode(opts)
-  local formatter = modes[mode]
-
-  -- if someone enters an invalid mode, default to symbol
-  if formatter == nil then
-    formatter = modes["symbol"]
-  end
-
-  return formatter(kind)
-end
-
 local function abbreviateString(str, maxwidth, ellipsis_char)
   if vim.fn.strchars(str) > maxwidth then
-    str = vim.fn.strcharpart(str, 0, maxwidth) .. (ellipsis_char ~= nil and ellipsis_char or "")
+    str = vim.fn.strcharpart(str, 0, maxwidth) .. ellipsis_char
   end
 
   return str
@@ -211,7 +115,7 @@ function lspkind.cmp_format(opts)
         .. ((opts.show_labelDetails and vim_item.menu ~= nil) and vim_item.menu or "")
     end
 
-    local ellipsis_char = opts.ellipsis_char
+    local ellipsis_char = opts.ellipsis_char ~= nil and opts.ellipsis_char or ""
 
     if opts.maxwidth.menu then
       local maxwidth = opts.maxwidth.menu
